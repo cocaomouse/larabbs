1ae3a05942e02d93a94e4101093ef3778be631f4
COM <cocaomouse@gmail.com>
2021-07-31 15:31:52 +0000

    用户相关

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    protected $width = 416;
+
+    public function __construct()
+    {
+        //在 __construct 方法中调用了 middleware 方法，该方法接收两个参数，第一个为中间件的名称，第二个为要进行过滤的动作。
+        $this->middleware('auth',['except' => ['show']]);
+    }
+
     //
     public function show(User $user)
     {
@@ -16,12 +25,25 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        //authorize 方法接收两个参数，第一个为授权策略的名称，第二个为进行授权验证的数据。
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request,User $user)
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploader)
     {
-        $user->update($request->all());
+        $this->authorize('update', $user);
+
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar,'avatars',$user->id,$this->width);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
 
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
