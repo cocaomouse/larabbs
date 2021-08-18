96a5a4a81b5430ab8da102750640edd0e72e0f63
com <cocaomouse@gmail.com>
2021-08-18 10:11:32 +0000

    优化侧边栏用户查询

--- app/Models/Traits/ActiveUserHelper.php
+++ app/Models/Traits/ActiveUserHelper.php
@@ -4,10 +4,12 @@ namespace App\Models\Traits;
 
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
 use Carbon\Carbon;
 use Cache;
 use DB;
 use Arr;
+use Spatie\Permission\Models\Permission;
 
 trait ActiveUserHelper
 {
@@ -28,7 +30,7 @@ trait ActiveUserHelper
     {
         // 尝试从缓存中取出 cache_key 对应的数据。如果能取到，便直接返回数据。
         // 否则运行匿名函数中的代码来取出活跃用户数据，返回的同时做了缓存。
-        return Cache::remember($this->cache_key,$this->cache_expire_in_seconds,function (){
+        return Cache::remember($this->cache_key, $this->cache_expire_in_seconds, function () {
             return $this->calculateActiveUsers();
         });
     }
@@ -46,72 +48,78 @@ trait ActiveUserHelper
         $this->calculateTopicScore();
         $this->calculateReplyScore();
 
-        // 数组按照得分排序
-        $users = Arr::sort($this->users, function ($user) {
-            return $user['score'];
-        });
-
-        // 我们需要的是倒序，高分靠前，第二个参数为保持数组的 KEY 不变
-        $users = array_reverse($users, true);
-
-        // 只获取我们想要的数量
-        $users = array_slice($users, 0, $this->user_number, true);
-
-        // 新建一个空集合
-        $active_users = collect();
-
-        foreach ($users as $user_id => $user) {
-            // 找寻下是否可以找到用户
-            $user = $this->find($user_id);
-
-            // 如果数据库里有该用户的话
-            if ($user) {
-                // 将此用户实体放入集合的末尾
-                $active_users->push($user);
-            }
+        if (!$this->users) {
+            return collect([]);
         }
 
-        // 返回数据
-        return $active_users;
+        // 数组按照得分排序
+        // 只获取我们想要的数量
+        return collect($this->users)->sortByDesc(function ($user) {
+            return $user['score'];
+        })->slice(0,$this->user_number)->map(function ($item) {
+            return (object)$item; //将数组转换成对象，前端模版无需修改
+        });
+
     }
 
     private function calculateTopicScore()
     {
         // 从话题数据表里取出限定时间范围（$pass_days）内，有发表过话题的用户
         // 并且同时取出用户此段时间内发布话题的数量
-        $topic_users = Topic::query()->select(DB::raw('user_id,count(*) as topic_count'))
-            ->where('created_at','>=',Carbon::now()->subDays($this->pass_days))
-            ->groupBy('user_id')
+        $topic_users = User::withCount(['topics' => function ($query) {
+            $query->where('created_at', '>=', Carbon::now()->subDays($this->pass_days));
+        }])
+            ->orderBy('topics_count', 'desc')
             ->get();
 
         // 根据话题数量计算得分
-        foreach ($topic_users as $value) {
-            $this->users[$value->user_id]['score'] = $value->topic_count * $this->topic_weight;
+        if ($topic_users->isNotEmpty()) {
+            $topic_users->map(function ($topic_user) {
+                $topic_score = $topic_user->topic_count * $this->topic_weight;
+                if (isset($this->user[$topic_user->id])) {
+                    $this->users[$topic_user->id]['score'] += $topic_score;
+                } else {
+                    $this->users[$topic_user->id]['score'] = $topic_score;
+                }
+                $this->users[$topic_user->id]['avatar'] = $topic_user->avatar;
+                $this->users[$topic_user->id]['name'] = $topic_user->name;
+                $this->users[$topic_user->id]['id'] = $topic_user->id;
+                return $this->users;
+            });
         }
+
     }
 
     private function calculateReplyScore()
     {
         // 从回复数据表里取出限定时间范围（$pass_days）内，有发表过回复的用户
         // 并且同时取出用户此段时间内发布回复的数量
-        $reply_users = Reply::query()->select(DB::raw('user_id,count(*) as reply_count'))
-            ->where('created_at','>=',Carbon::now()->subDays($this->pass_days))
-            ->groupBy('user_id')
+        $reply_users = User::withCount(['replies' => function ($query) {
+            $query->where('created_at', '>=', Carbon::now()->subDays($this->pass_days));
+        }])
+            ->orderBy('replies_count', 'desc')
             ->get();
 
         // 根据回复数量计算得分
-        foreach ($reply_users as $value) {
-            if (isset($this->users[$value->user_id])) {
-                $this->users[$value->user_id]['score'] += $value->reply_count * $this->reply_weight;
-            } else {
-                $this->users[$value->user_id]['score'] = $value->reply_count;
-            }
+        if ($reply_users->isNotEmpty()) {
+            $reply_users->map(function ($reply_user) {
+                $reply_score = $reply_user->replies_count * $this->reply_weight;
+                if (isset($this->users[$reply_user->id])) {
+                    $this->users[$reply_user->id]['score'] += $reply_score;
+                } else {
+                    $this->users[$reply_user->id]['score'] = $reply_score;
+                }
+                $this->users[$reply_user->id]['avatar'] = $reply_user->avatar;
+                $this->users[$reply_user->id]['name'] = $reply_user->name;
+                $this->users[$reply_user->id]['id'] = $reply_user->id;
+                return $this->users;
+            });
         }
     }
 
     private function cacheActiveUsers($active_users)
     {
         // 将数据放入缓存中
-        Cache::put($this->cache_key,$active_users,$this->cache_expire_in_seconds);
+        Cache::put($this->cache_key, $active_users, $this->cache_expire_in_seconds);
     }
 }
